57c9ced09034d2198c8ab8acd0809a9d796e640c
Govani13 <137343105+Govani13@users.noreply.github.com>
2025-04-24 21:07:32 -0300

    agregar detalles

--- funciones/secretario/gestion.py
+++ funciones/secretario/gestion.py
@@ -1,36 +1,4 @@
-import re 
-import from datos matriz_pacientes, matriz_turnos
 
-#funcion validar dni 
-def validar_dni(dni):
-    patron = r"^\d{7,8}$"
-    coincidencia = re.match(patron, dni)
-    if coincidencia is not None:
-        return True
-    else:
-        return False
-
-def buscar_id_paciente (matriz_pacientes, dni):
-    for paciente in matriz_pacientes:
-        if paciente['dni'] == dni:
-            return paciente['id']
-    return None
-
-def chequear_turno (matriz_turnos, dia, hora, dni):
-    for fila in matriz_turnos:
-        if fila[1].lower() == dia and fila[2]== hora and fila[3]==dni and fila[4]== 'ocupado':
-          existencia= 1
-        elif fila[1].lower() == dia and fila[2]== hora and fila[3]==dni and fila[4]== 'disponible':
-            existencia = 2
-        else:
-            existencia= 0
-    return existencia   
-
-def buscar_id_turno(documento):
-     for fila in matriz_turnos:
-         if fila[1].lower() == dia and fila[2]== hora and fila[3]==dni and fila[4]== 'ocupado
-             id_turno= fila[0]
-    return id_turno
 
 def vaciar_turno(matriz):
     turno_vaciar=int(input("ingrese el id del turno a eliminar:"))
